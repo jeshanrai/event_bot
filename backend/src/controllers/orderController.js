1f6a4af252886fd16e05ea2542c0b9ba516a08b2
harryv23 <narahari.sijapati@ibriz.ai>
2026-02-09 22:46:38 +0545

    completed status added a s valid status

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -48,6 +48,7 @@ const updateOrderStatus = async (req, res) => {
       "preparing",
       "ready",
       "delivered",
+      "completed",
       "cancelled",
     ];
     if (!validStatuses.includes(status)) {
